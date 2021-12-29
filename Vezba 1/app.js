7979ed20ec55e6aeef45ca23b518164f509450c4
EnesDaca <enesdaca@hotmail.com>
2021-12-29 18:49:23 +0100

    js

---
+++ Vezba 1/app.js
@@ -0,0 +1,15 @@
+// var1 = prompt ('Unesi broj 1:')
+
+// var2 = prompt ('Unesi broj 2')
+
+// var3 = (var1 + var2)
+
+// console.log(var1 + var2)
+
+// zbirBrojeva = (var1 + var2)
+
+a = prompt ('Unesite broj: ');
+
+console.log (a % 2 === 0);
+
+
---
+++ vezba 2/app.js
@@ -0,0 +1,171 @@
+// brojGodina = prompt("Unesi koliko imas godina");
+
+// console.log(brojGodina);
+
+// console.log(1 > 5);
+// console.log(7 > 5);
+// console.log(5 >= 5);
+// console.log(4 <= 5);
+
+// console.log(5 == "5");
+// console.log(5 === "5");
+
+// console.log("D" === "D");
+// console.log("D" === "D");
+
+// console.log(Boolean(0));
+// console.log(Boolean(5));
+
+// console.log(Boolean(""));
+// console.log(Boolean("ASDFGHJKL"));
+
+// var1 = true;
+
+// var2 = false;
+
+// null
+
+// console.log(Boolean(NaN));
+
+// && AND
+
+// || OR
+
+// false || false === false
+// true || false === true
+// true || true ===true
+// false||true === true
+
+// true && true === true;
+// false && false === false;
+// false && true === false;
+// true && false === false;
+
+// a = 5;
+// b = 10;
+
+// console.log(a < b);
+
+// a = prompt("Unesi broj izmedju 1 i 10");
+
+// console.log(a < 10 && a > 1 );
+
+// console.log(true || (false && true));
+
+// a = prompt("Unesi broj ");
+
+// console.log(a % 2 === 0);
+
+// a = 5;
+
+// b = 3;
+
+// c = a === b;
+
+// console.log(c);
+
+// console.log("Daris".length);
+
+// a = "Daris";
+// console.log(a.length);
+
+// ime = prompt("Unesi ime");
+// duzina = ime.length;
+
+// console.log("Duzina vaseg imena je : ", duzina);
+
+// ime = "Daris";
+//     01234
+// console.log(ime[5]);
+
+// a = "Daris";
+// console.log(a.toUpperCase());
+// console.log(a.toLowerCase());
+// console.log(a.concat("Nesto"));
+// console.log("Darisa".indexOf("a"));
+// console.log("Darisa".includes("g"));
+
+// str = "Apple, Banana, Kiwi";
+// part = str.slice(7, 13);
+
+// console.log(part);
+
+// str = "Apple, Banana, Kiwidsgfhghkjlkl";
+// part = str.slice(7);
+
+// console.log(part);
+
+// text = "Please visit Microsoft!";
+
+// newText = text.replace("Microsoft", "W3Schools");
+
+// console.log(newText);
+
+// text = "kiwi,banana,daris,kiwi";
+
+// console.log(text.split(","));
+
+// a = 5 + 2;
+
+// console.log("Ovaj broj je", a, "i on je u stringu");
+// console.log(`Ovaj broj je ${a} i on je u stringu`);
+
+// a = Math.random() * 100;
+
+// console.log(Math.round(a));
+
+// if (5 > 2) {
+//   console.log("tacno je");
+// }
+
+// console.log("ostali code");
+// console.log("ostali code");
+
+// a = 150;
+// b = 20;
+
+// if (a > b) {
+//   console.log("A je vece");
+// }
+
+// if (b > a) {
+//   console.log("B je vece");
+// }
+
+// a = prompt("Unesi broj 1-10: ");
+
+// if (a > 1 && a < 10) {
+//   console.log("Unos je tacan");
+// }
+
+// if (a < 1 || a > 10) {
+//   console.log("Unos je POGRESAN");
+// }
+
+// if (5 > 1) {
+//   console.log("IF JE TACAN");
+// } else {
+//   console.log("IF SE NIJE IZVRSI,IZVRSIO SE ELSE");
+// }
+
+//-----------------------------------------
+//Domaci:
+//Napisati program da proveri da li je uneti string "dupliran",EXAMPLE:
+// "darisdaris"
+// console.log("daris" === "daris");
+//-----------------------------------------
+
+
+a = prompt('unesi broj');
+
+if (a<=12) {
+    console.log(`${a} jutro je`);
+}
+
+if (a>=13) {
+    console.log(`${a} dan je`);
+}
+
+if  (a>=20) {
+    console.log(`${a} noc je`);
+}
