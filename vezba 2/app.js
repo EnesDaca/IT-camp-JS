fdbf05883f5409d9f0f416e995450c59a79321f7
EnesDaca <enesdaca@hotmail.com>
2022-01-29 21:04:50 +0100

    js

--- vezba 2/app.js
+++ vezba 2/app.js
@@ -1081,10 +1081,10 @@
 // ==================================================
 
 
-  function boolToWord ( bool) {
-      if bool {
-        return "Yes"
-      }else {
-        return "No"
-      }
-    }
+  // function boolToWord ( bool) {
+  //     if bool {
+  //       return "Yes"
+  //     }else {
+  //       return "No"
+  //     }
+  //   }
